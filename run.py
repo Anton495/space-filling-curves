2d34bf9a2a14c929aa20981338adc0e99a513e38
Anton Korneev <45163732+Anton495@users.noreply.github.com>
2018-11-23 14:06:31 +0300

    Update run.py

--- run.py
+++ run.py
@@ -4,30 +4,26 @@ from pl_curves import plcurves
 #Indicate subdivision number and run
 k = 3
 
-#Choose the curve and run
+get_Curve =[#2d curve examples (mono-fractal)
+            get_Hilbert_Curve(k),        # 0
+            get_Peano_Curve(k),          # 1
+            get_Meurthe_Curve_Curve(k),  # 2
+            get_Coil_Curve_Curve(k),     # 3
+            get_Serpentine_Curve(k),     # 4
+            get_R_Curve(k),              # 5
+            #2d curve examples (bi-fractal)
+            get_Beta_Omega_Curve(k),     # 6
+            #2d curve examples (tetra-fractal)
+            get_ARW_Curve(k),            # 7
+            #3d curve examples (mono-fractal)
+            get_Tokarev_Curve(k),        # 8 
+            get_Haverkort_Curve_1(k),    # 9
+            get_Haverkort_Curve_2(k),    # 10
+            #3d curve examples (bi-fractal)
+            get_Neptunus_Curve(k),       # 11
+            get_Luna_Curve(k)]           # 12
 
-#2d curve examples (mono-fractal)
-curve = get_Hilbert_Curve(k)
-#curve = get_Peano_Curve(k)
-#curve = get_Meurthe_Curve_Curve(k)
-#curve = get_Coil_Curve_Curve(k)
-#curve = get_Serpentine_Curve(k)
-#curve = get_R_Curve(k)
-
-#2d curve examples (bi-fractal)
-#curve = get_Beta_Omega_Curve(k)
-
-#2d curve examples (quater-fractal)
-#curve = get_ARW_Curve(k)
-
-#3d curve examples (mono-fractal)
-#curve = get_Tokarev_Curve(k)
-#curve = get_Haverkort_Curve_1(k)
-#curve = get_Haverkort_Curve_2(k)
-
-#3d curve examples (bi-fractal)
-#curve = get_Neptunus_Curve(k)
-#curve = get_Luna_Curve(k)
-
+#Indicate the curve number and run
+curve = get_Curve[0]
 
 plcurves(curve.subdiv_n, curve.dim, curve.genus, curve.number_sub)
