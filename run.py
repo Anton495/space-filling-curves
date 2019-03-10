3a134859289d5e32ff929da34bd46770ba408e91
Anton Korneev <45163732+Anton495@users.noreply.github.com>
2019-03-10 18:12:34 +0300

    Update run.py

--- run.py
+++ run.py
@@ -1,29 +1,29 @@
 from examples import *
 from pl_curves import plcurves
 
-#Indicate subdivision number and run
-k = 3
+#Indicate subdivision number,curve number and run
+subdiv_number = 2
+curve_number = 1
 
 get_Curve =[#2d curve examples (mono-fractal)
-            get_Hilbert_Curve(k),        # 0
-            get_Peano_Curve(k),          # 1
-            get_Meurthe_Curve(k),  # 2
-            get_Coil_Curve(k),     # 3
-            get_Serpentine_Curve(k),     # 4
-            get_R_Curve(k),              # 5
+            get_Hilbert_Curve(),         # 0
+            get_Peano_Curve(),           # 1
+            get_Meurthe_Curve_Curve(),   # 2
+            get_Coil_Curve_Curve(),      # 3
+            get_Serpentine_Curve(),      # 4
+            get_R_Curve(),               # 5
             #2d curve examples (bi-fractal)
-            get_Beta_Omega_Curve(k),     # 6
+            get_Beta_Omega_Curve(),      # 6
             #2d curve examples (tetra-fractal)
-            get_ARW_Curve(k),            # 7
+            get_ARW_Curve(),             # 7
             #3d curve examples (mono-fractal)
-            get_Tokarev_Curve(k),        # 8 
-            get_Haverkort_Curve_1(k),    # 9
-            get_Haverkort_Curve_2(k),    # 10
+            get_Tokarev_Curve(),         # 8 
+            get_Haverkort_Curve_1(),     # 9
+            get_Haverkort_Curve_2(),     # 10
             #3d curve examples (bi-fractal)
-            get_Neptunus_Curve(k),       # 11
-            get_Luna_Curve(k)]           # 12
+            get_Neptunus_Curve(),        # 11
+            get_Luna_Curve()]            # 12
 
-#Indicate the curve number and run
-curve = get_Curve[0]
+curve = get_Curve[curve_number]
 
-plcurves(curve.subdiv_n, curve.dim, curve.genus, curve.number_sub)
+plcurves(curve.get_subdiv_n(subdiv_number), curve.dim, curve.genus, subdiv_number)
