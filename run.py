ec42bb4532cef0e5db128f5ad033bb274eda0a4b
Anton Korneev <45163732+Anton495@users.noreply.github.com>
2019-03-15 17:43:31 +0300

    Update run.py

--- run.py
+++ run.py
@@ -1,29 +1,29 @@
 from examples import *
-from pl_curves import plcurves
+from pl_curves import plot_curve
 
 #Indicate subdivision number,curve number and run
-subdiv_number = 2
-curve_number = 1
+subdiv_number = 4
+curve_number = 7
 
-get_Curve =[#2d curve examples (mono-fractal)
-            get_Hilbert_Curve(),         # 0
-            get_Peano_Curve(),           # 1
-            get_Meurthe_Curve_Curve(),   # 2
-            get_Coil_Curve_Curve(),      # 3
-            get_Serpentine_Curve(),      # 4
-            get_R_Curve(),               # 5
+get_curve =[#2d curve examples (mono-fractal)
+            get_hilbert_curve(),         # 0
+            get_peano_curve(),           # 1
+            get_meurthe_curve(),         # 2
+            get_coil_curve(),            # 3
+            get_serpentine_curve(),      # 4
+            get_R_curve(),               # 5
             #2d curve examples (bi-fractal)
-            get_Beta_Omega_Curve(),      # 6
+            get_beta_Omega_curve(),      # 6
             #2d curve examples (tetra-fractal)
-            get_ARW_Curve(),             # 7
+            get_ARW_curve(),             # 7
             #3d curve examples (mono-fractal)
-            get_Tokarev_Curve(),         # 8 
-            get_Haverkort_Curve_1(),     # 9
-            get_Haverkort_Curve_2(),     # 10
+            get_tokarev_curve(),         # 8 
+            get_haverkort_curve_1(),     # 9
+            get_haverkort_curve_2(),     # 10
             #3d curve examples (bi-fractal)
-            get_Neptunus_Curve(),        # 11
-            get_Luna_Curve()]            # 12
+            get_neptunus_curve(),        # 11
+            get_luna_curve()]            # 12
 
-curve = get_Curve[curve_number]
+curve = get_curve[curve_number]
 
-plcurves(curve.get_subdiv_n(subdiv_number), curve.dim, curve.genus, subdiv_number)
+plot_curve(curve.get_subdiv(subdiv_number), curve.dim, curve.genus, subdiv_number)
