9b3420d297022abf50b278e32acee63de7a03b05
Anton Korneev <45163732+Anton495@users.noreply.github.com>
2019-03-15 17:46:50 +0300

    Update Sierpinski_Knopp_Curve.py

--- triangular curve/Sierpinski_Knopp_Curve.py
+++ triangular curve/Sierpinski_Knopp_Curve.py
@@ -3,10 +3,10 @@ import os
 sys.path.append(os.path.dirname(sys.argv[0])+'/..')
 
 import numpy as np
-from pl_curves import plcurves
+from pl_curves import plot_curve
 
 #Indicate subdivision number and run
-k = 4
+k = 2
 
 #The definition of basis vectors and the origin
 O = np.array([[0,0]])
@@ -35,7 +35,7 @@ for n in range(k):
 subdiv_n = np.concatenate([subdiv_n,iJ,-subdiv_n])
 
 #Generating, scaling and shifting the curve coordinates
-subdiv_n = np.cumsum(np.concatenate([O,subdiv_n]),axis = 0)/2**(k+2)
-subdiv_n = np.column_stack((subdiv_n[:,0],subdiv_n[:,1] + subdiv_n[1,1])) + 1/2**(k+3)
+subdiv_n = np.cumsum(np.concatenate([O,subdiv_n]),axis = 0)/2 #/2**(k+2)
+subdiv_n = np.column_stack((subdiv_n[:,0],subdiv_n[:,1] + subdiv_n[1,1])) #+ 1/2**(k+3)
 
-plcurves(subdiv_n, 2, 4, k)
+plot_curve(subdiv_n, 2, 4, k)
